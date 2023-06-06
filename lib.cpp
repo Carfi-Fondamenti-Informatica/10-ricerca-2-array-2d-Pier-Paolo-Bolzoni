3f4ac01257fca63b8049c8b77db75a9e63af90dd
Pier-Paolo-Bolzoni <100352364+Pier-Paolo-Bolzoni@users.noreply.github.com>
2023-06-06 18:33:53 +0200

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,22 @@
-
+#include <iostream> 
+#include "lib.h" 
+using namespace std; 
+ 
+void init (char mat[], int n){ 
+    for(int i=0;i<n;i++){ 
+        mat[i]= ' '; 
+    } 
+} 
+ 
+void init(char mat[5][15]){ 
+    for(int i=0;i<5;i++){ 
+        init(mat[i],15); 
+    } 
+} 
+ 
+void inserimento(char nomi[5][15], char cognomi[5][15]){ 
+    for(int i=0; i<5;i++){ 
+        inserimento(nomi[i],15); 
+        inserimento(cognomi[i],15); 
+    } 
+}
