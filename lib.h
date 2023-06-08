43cbb840bdf2eaeecfc876b63322b3cf7957e4d0
Pier-Paolo-Bolzoni <100352364+Pier-Paolo-Bolzoni@users.noreply.github.com>
2023-06-08 12:04:29 +0200

    Update lib.h

--- lib.h
+++ lib.h
@@ -1,5 +1,4 @@
-void init(char mat[5][15]);
-void inserimento(char a[]);
-void inserimento(char nomi[5][15], char cognomi[5][15]);
-int ricerca(char nomi[][15], char cognomi[][15], char nome[], char cognome[]);
-
+void init(char [][15]);
+void inserimento(char a[],int);
+void inserimento(char [5][15], char [5][15]);
+int ricerca(char [][15], char [][15], char [], char []);
