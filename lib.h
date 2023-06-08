311229e4a8f9b701e180ce77a67da6760e17519a
Pier-Paolo-Bolzoni <100352364+Pier-Paolo-Bolzoni@users.noreply.github.com>
2023-06-08 11:56:10 +0200

    Update lib.h

--- lib.h
+++ lib.h
@@ -1,3 +1,5 @@
-void init (char mat[], int n); 
-void init(char mat[5][15]); 
+void init(char mat[5][15]);
+void inserimento(char a[]);
 void inserimento(char nomi[5][15], char cognomi[5][15]);
+int ricerca(char nomi[][15], char cognomi[][15], char nome[], char cognome[]);
+
